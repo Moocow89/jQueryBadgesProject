c634f6cdaf2552c515337592cc848c48bb9b217b
amanda benhamou <amandabenhamou@gmail.com>
2016-12-07 23:31:16 +0000

    Solved Project

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,33 @@
 $(function() {
+    
 
-  // your code will go here
+ $.ajax({
+      url: 'https://www.codeschool.com/users/MooCow89.json',
+      dataType: 'jsonp', 
+      success: function(response){
+             addCourses(response.courses.completed);
+                
+      }
+    });
+    
+    function addCourses(courses){
+        courses.forEach(function(course){
+            var courseElement = $("<div>");
+            var courseTitle = $("<h3>");
+            var courseImg = $("<img>");
+            var courseLink = $("<a>");
+            courseLink.attr("href", course.url);
+            courseLink.attr("target", "_blank");
+            courseLink.addClass("btn btn-primary");
+            courseLink.text("See Course");
+            courseImg.attr("src", course.badge);
+            courseTitle.text(course.title);
+            courseElement.className("course");
+            courseElement.append(courseTitle);
+            courseElement.append(courseImg);
+            courseElement.append(courseLink);
+            $("#badges").append(courseElement);
+        })
+    }
 
 });
